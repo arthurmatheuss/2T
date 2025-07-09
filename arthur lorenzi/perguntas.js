8c0727fa09c1d7ffe14637fcac549a07b6159650
arthurmatheuss <arthur.lorenzi@escola.pr.gov.br>
2025-07-09 07:35:13 -0300

    Add files via upload

---
+++ arthur lorenzi/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual a capital da Russia?',
+    'A capital da Russia é Moscou'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Geografia',
+    'Aonde fica o canal do Panama?',
+    'O canal do Panama fica na america central no Panama'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a maior ilha do mundo?',
+    'Groenlândia é a maior ilha do mundo'
+)
